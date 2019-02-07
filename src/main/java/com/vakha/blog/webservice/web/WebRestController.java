c2a8ff94b69641d65e1ded3501fd6cbe86f28a46
vakha <bestcure9@gmail.com>
2019-02-07 17:45:41 +0900

    test initial

---
+++ src/main/java/com/vakha/blog/webservice/web/WebRestController.java
@@ -0,0 +1,26 @@
+package com.vakha.blog.webservice.web;
+
+import com.vakha.blog.webservice.dto.posts.PostsSaveRequestDto;
+import com.vakha.blog.webservice.domain.posts.PostsRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class WebRestController {
+
+    private PostsRepository postsRepository;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "HelloWorld";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto){
+        postsRepository.save(dto.toEntity());
+    }
+}
